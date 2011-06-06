4c33fb6867c081dd6e6c11fad778f034d8c436e7
Stefan Seemayer <stefan@seemayer.de>
2011-06-06 18:54:26 +0200

    + Add barriers to sshgrid-worker + Add example command file with barriers * Fixed sshgrid-do duplicate parameter naming

---
+++ share/SSHGrid/examples/command_files/barriers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# barriers.sh - Barrier crossing sample 
+echo -e "start\tA   0\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\tA   0\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\tA   1\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\tA   1\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\tA   2\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\tA   2\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\tA   3\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\tA   3\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\tA   4\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\tA   4\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\tA   5\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 5; echo -e "stop\tA   5\t`pwd`\t`hostname`\t`date`" >> result_echos
+#%B Barrier
+echo -e "start\t B  0\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t B  0\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t B  1\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t B  1\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t B  2\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t B  2\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t B  3\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t B  3\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t B  4\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t B  4\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t B  5\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 10; echo -e "stop\t B  5\t`pwd`\t`hostname`\t`date`" >> result_echos
+#%B Barrier
+echo -e "start\t  C 0\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t  C 0\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t  C 1\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t  C 1\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t  C 2\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 7; echo -e "stop\t  C 2\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t  C 3\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t  C 3\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t  C 4\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t  C 4\t`pwd`\t`hostname`\t`date`" >> result_echos
+echo -e "start\t  C 5\t`pwd`\t`hostname`\t`date`" >> result_echos; sleep 1; echo -e "stop\t  C 5\t`pwd`\t`hostname`\t`date`" >> result_echos
